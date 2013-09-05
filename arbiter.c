29ff5f0b66cf2d49f84ddbfb909e591fad7ca7b3
Jonathan Heathcote <mail@jhnet.co.uk>
2013-09-05 12:26:28 +0100

    Added untested arbiter.

---
+++ arbiter.c
@@ -0,0 +1,139 @@
+/**
+ * TickySim -- A timing based interconnection network simulator.
+ *
+ * arbiter.c -- A round-robbin arbiter which at a regular period attempts to
+ * forward values from a set of input buffers into a single output buffer using
+ * a round-robbin approach in the case of contention.
+ */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "scheduler.h"
+#include "buffer.h"
+#include "arbiter.h"
+
+
+/******************************************************************************
+ * Internal datastructures.
+ ******************************************************************************/
+
+/**
+ * The structure representing a particular arbiter. Includes the index of the
+ * last input handled.
+ *
+ * The field handle_input is a bool set by the tick function and specifies
+ * whether a value from the input is to be forwarded by the tock function. If it
+ * is true, the input indicated by last_input will have its value forwarded to
+ * the output.
+ */
+typedef struct arbiter {
+	buffer_t **inputs;
+	size_t     num_inputs;
+	buffer_t  *output;
+	
+	size_t last_input;
+	
+	bool handle_input;
+} arbiter_t;
+
+
+/******************************************************************************
+ * Private functions.
+ ******************************************************************************/
+
+/**
+ * Internal function.
+ *
+ * Arbiter "tick" callback. Checks to see if a value should be forwarded.
+ */
+void
+arbiter_tick(void *a_)
+{
+	arbiter_t *a = (arbiter_t *)a_;
+	
+	// Immediately stop if the output is blocked
+	if (buffer_is_full(a->output))
+		return;
+	
+	// Iterate over all the inputs starting after the last one to be handled
+	for ( int i = a->last_input + 1
+	    ; i != a->last_input
+	    ; i = (i+1)%a->num_inputs
+	    ) {
+		if (!buffer_is_empty(a->inputs[i])) {
+			// There is a value ready at this input, set it to be forwarded during the
+			// tock phase.
+			a->last_input   = i;
+			a->handle_input = true;
+			return;
+		}
+	}
+	
+	// No inputs were ready, do nothing!
+}
+
+/**
+ * Internal function.
+ *
+ * Arbiter "tock" callback. Forwards a packet if requested by tick.
+ */
+void
+arbiter_tock(void *a_)
+{
+	arbiter_t *a = (arbiter_t *)a_;
+	
+	if (a->handle_input) {
+		void *value = buffer_pop(a->inputs[a->last_input]);
+		buffer_push(a->output, value);
+		
+		a->handle_input = false;
+	}
+}
+
+
+/******************************************************************************
+ * Public functions.
+ ******************************************************************************/
+
+void
+arbiter_create( scheduler_t *s
+              , ticks_t      period
+              , buffer_t   **inputs
+              , size_t       num_inputs
+              , buffer_t    *output
+              )
+{
+	arbiter_t *a = malloc(sizeof(arbiter_t));
+	assert(a != NULL);
+	
+	// Copy the input array into a local copy
+	a->inputs = calloc(num_inputs, sizeof(buffer_t *));
+	assert(a->inputs != NULL);
+	memcpy(a->inputs, inputs, num_inputs * sizeof(buffer_t *));
+	
+	a->num_inputs = num_inputs;
+	
+	a->output = output;
+	
+	// Initialised so that if the inputs are immediately contended, input 0 will
+	// go first. This isn't neccessary (since the decision is arbitary) but won't
+	// surprise someone watching progress externally.
+	a->last_input = num_inputs;
+	
+	a->handle_input = false;
+	
+	// Schedule the arbiter tick/tock functions to occur at the specified
+	// interval.
+	scheduler_schedule( s, period
+	                  , arbiter_tick, (void *)a
+	                  , arbiter_tock, (void *)a
+	                  );
+}
+
+
+
+
---
+++ arbiter.h
@@ -0,0 +1,37 @@
+/**
+ * TickySim -- A timing based interconnection network simulator.
+ *
+ * arbiter.h -- A round-robbin arbiter which at a regular period attempts to
+ * forward values from a set of input buffers into a single output buffer using
+ * a round-robbin approach in the case of contention.
+ */
+
+#ifndef ARBITER_H
+#define ARBITER_H
+
+#include <stdlib.h>
+
+#include "scheduler.h"
+#include "buffer.h"
+
+
+/**
+ * Create a new arbiter. Adds itself to the scheduler with the requesteed period.
+ *
+ * @param scheduler The scheduler controling the simulation.
+ * @param period The period at which the arbiter will attempt to forward one
+ *               value.
+ * @param inputs An array of input buffers to arbitrate between. This array will
+ *               be copied.
+ * @param num_inputs The length of the inputs array.
+ * @param output The output buffer into which values should be placed.
+ */
+void arbiter_create( scheduler_t *scheduler
+                   , ticks_t      period
+                   , buffer_t   **inputs
+                   , size_t       num_inputs
+                   , buffer_t    *output
+                   );
+
+
+#endif
